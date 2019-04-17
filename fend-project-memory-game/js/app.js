d902742c78f933c2ac3c88506b97376da115ead3
omarn75 <48870362+omarn75@users.noreply.github.com>
2019-04-16 20:10:19 -0400

    Update app.js

--- fend-project-memory-game/js/app.js
+++ fend-project-memory-game/js/app.js
@@ -1,4 +1,4 @@
-// * Create a list that holds all of your cards
+// * Create a list that holds all of your cards twice for a match
 const icons = ["fa fa-diamond","fa fa-paper-plane-o",
  "fa fa-anchor", "fa fa-bolt", "fa fa-cube", "fa fa-leaf",
  "fa fa-bicycle", "fa fa-bomb", "fa fa-diamond","fa fa-paper-plane-o",
